65dc0f28d01f298f89fceed48324ac1ac6cbba3d
Sofia Panchaud <s.panchaud.89@gmail.com>
2026-01-26 12:17:44 -0500

    add filter for number of days to view on chart in Chart component

--- components/Chart/ChartContainer.tsx
+++ components/Chart/ChartContainer.tsx
@@ -9,6 +9,7 @@ const dateFormatter = (date:Date) => {
 const ChartContainer = () => {
   const {entries} = useEntries();
   const [daysToFilter, setDaysToFilter] = useState(7);
+  const [filterValue, setFilterValue] = useState(daysToFilter);
 
   const todayMs = new Date().getTime();
   const startDateMs = todayMs - (daysToFilter * 24 * 60 * 60 * 1000);
@@ -22,9 +23,20 @@ const ChartContainer = () => {
     const date = dateFormatter(new Date(entry.date));
     return {date: date, mood: entry.moodRating}
   });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const filterValue = e.target.dayFilter.value;
+    setDaysToFilter(filterValue);
+  }
   return(
     <div className="w-1/2 flex items-center justify-evenly flex-col">
       <h2>Mood Data</h2>
+      <form id="filter-settings" onSubmit={handleSubmit}>
+        <label htmlFor="dayFilter">Days to View: </label>
+        <input id="dayFilter" type="number" value={filterValue} onChange={(e) => setFilterValue(Number(e.target.value))}></input>
+        <button type="submit">Apply</button>
+      </form>
       <Chart data={chartEntries}/>
     </div>
   )
